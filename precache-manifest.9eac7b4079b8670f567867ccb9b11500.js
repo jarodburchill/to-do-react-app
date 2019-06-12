f3e533f3a422340ab6a5507bae5db6d58b6e8a64
BURCHILL <JBURCHILL@wrps.on.ca>
2019-06-12 15:07:44 -0400

    Updates

---
+++ precache-manifest.9eac7b4079b8670f567867ccb9b11500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7094e82c6fca52c20c243a6d9ffc033",
+    "url": "/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "7d8be63af5a0ee04e205",
+    "url": "/ToDoReactApp/static/css/main.c13c40cc.chunk.css"
+  },
+  {
+    "revision": "f319fe71299dd408b275",
+    "url": "/ToDoReactApp/static/js/2.ad3586fe.chunk.js"
+  },
+  {
+    "revision": "7d8be63af5a0ee04e205",
+    "url": "/ToDoReactApp/static/js/main.06cdc1d9.chunk.js"
+  },
+  {
+    "revision": "22516846832dc06211c4",
+    "url": "/ToDoReactApp/static/js/runtime~main.52c04ec0.js"
+  }
+]);
